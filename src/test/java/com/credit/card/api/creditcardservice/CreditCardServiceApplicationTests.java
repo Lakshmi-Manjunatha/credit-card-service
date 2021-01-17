2208f56e2b26603f52c9c5149d08ded0c64141cf
Lakshmi-Manjunatha <lakshmijm12@gmail.com>
2021-01-17 16:00:47 +0000

    build  initial spring boot project setup

---
+++ src/test/java/com/credit/card/api/creditcardservice/CreditCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.card.api.creditcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
